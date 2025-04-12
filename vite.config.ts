ec8928466dc1a0f97ba226e8297924aa1532be1d
lp <9398429+clowder-of-cats@users.noreply.github.com>
2025-04-12 14:06:57 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/brew-oracle/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    assetsDir: 'assets'
+  }
 })
